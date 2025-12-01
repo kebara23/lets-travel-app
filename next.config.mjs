cdd2b9814d523e8b78db8c68c32de51e993bed91
kebara23 <kebara23@gmail.com>
2025-11-30 23:17:56 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Ignora errores de estilo en producción
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ignora errores de tipos en producción
     ignoreBuildErrors: true,
   },
 };
